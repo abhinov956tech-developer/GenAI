42b876217ac6c294ad1078f59c88af8d74976f6f
Abhinov Dutta <abhinavdutta927@gmail.com>
2025-04-22 01:08:13 +0530

    commit done

---
+++ chat.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel('gemini-1.5-flash')  
+response = model.generate_content('Why is the sky blue?')
+
+print(response.text)
